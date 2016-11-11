d2b3e067c5d122908b828b163e3e3af4f4303649
LanderVerschueren <verschueren@live.nl>
2016-11-11 22:48:43 +0100

    Push for desktop

--- New Technology/js/base.js
+++ New Technology/js/base.js
@@ -7,21 +7,21 @@ var GENERAL = {
 			data: {
 				message: 'Hello Vue!'
 			}
-		})
+		});
 
 		var app2 = new Vue({
 			el: "#app2",
 			data: {
 				id: 'inspect-me' /* Bind "id: 'inspect-me'" to <span v-bind:id="id"> */
 			}
-		})
+		});
 
 		var app3 = new Vue({
 			el: "#app3",
 			data: {
 				seen: true
 			}
-		})
+		});
 
 		var app4 = new Vue ({
 			el: "#app4",
@@ -32,7 +32,7 @@ var GENERAL = {
 					{ text: 'Build something awesome' }
 				]
 			}
-		})
+		});
 
 		var app5 = new Vue ({
 			el: "#app5",
@@ -44,19 +44,19 @@ var GENERAL = {
 					this.message = this.message.split('').reverse().join('')
 				}
 			}
-		})
+		});
 
 		var app6 = new Vue({
 			el: '#app6',
 			data: {
 				message: 'Hello Vue!'
 			}
-		})
+		});
 
 		Vue.component('todo-item', {
 			props: ['todo'],
 			template: '<li>{{ todo.text }}</li>'
-		})
+		});
 
 		var app7 = new Vue({
 			el: '#app7',
@@ -67,7 +67,29 @@ var GENERAL = {
 					{ text: 'Build something awesome' }
 				]
 			}
-		})
+		});
+
+		var myModel = {
+			name: "Ashley",
+			age: 24,
+			friends: [
+				{ name: "Bob", age: 21 },
+				{ name: "Jane", age: 20 },
+				{ name: "Anna", age: 29 }
+			]
+		};
+
+		var myViewModel = new Vue({
+			el: '#myView',
+			data: myModel,
+
+			//A click handler inside methods
+			methods: {
+				myClickHandler: function( e ) {
+					alert( "Hello " + this.name );
+				}
+			}
+		});
 	}
 };
 
